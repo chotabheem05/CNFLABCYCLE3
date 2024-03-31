981bf8d9062fb9bd84789ddd0f0e39bd937d4568
chotabheem05 <156694265+chotabheem05@users.noreply.github.com>
2024-03-31 10:59:21 +0530

    Add files via upload

---
+++ LabCycle3/src/main/java/com/example/LabCycle3/LabCycle3Application.java
@@ -0,0 +1,13 @@
+package com.example.LabCycle3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabCycle3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabCycle3Application.class, args);
+	}
+
+}
